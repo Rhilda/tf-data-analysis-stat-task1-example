c750a114d8fb0e256bb1fe5d49ea947d78f9f471
Rhilda <ildariismagilov@mail.ru>
2023-04-04 21:32:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    mean_count = np.mean(x)
     t = 10
-    L = mean_count / t
-    return L
+    n = len(x)
+    lmbd = x.sum() / (t*n)
+    return lmbd

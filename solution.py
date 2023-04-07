97f865166e0946fea424dddae0d8393c4f99a969
Rhilda <ildariismagilov@mail.ru>
2023-04-08 00:10:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,8 @@ import numpy as np
 chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    errors = np.random.normal(loc=-47, scale=np.exp(1), size=x.shape)
+    # Генерация случайных ошибок измерения скорости
+    errors = np.random.normal(loc=-21, scale=np.exp(1), size=x.shape)
 
     # Средняя скорость машин за 10 секунд с учетом ошибок измерения
     v_mean = np.mean(x + errors)
@@ -18,3 +19,5 @@ def solution(x: np.array) -> float:
 
     # Точечная оценка коэффициента ускорения
     a = (v_mean - v_0) / t
+
+    return a
